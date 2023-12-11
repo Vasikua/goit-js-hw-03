9952c57b9d9b2cbe521624ea5b947bb40d85e53b
Vasyl Mandiak <Vasik_ua@ukr.net>
2023-12-10 20:15:59 -0600

    task - 3 done

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,11 @@
-function name(params) {
-    
+function filterArray(numbers, value) {
+    let newArray = [];
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] > value) {
+            newArray.push(numbers[i]);
+        }
+    }
+    return newArray;
 }
 
 
